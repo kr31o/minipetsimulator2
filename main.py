ed956e1d1a7f9938f84a1f0e921a2848170fea9d
kr31o <abok52806@gmail.com>
2026-04-16 14:21:05 +0300

    add play_with_pet feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pet import Pet
 
-pet = Pet("cat", 30)
+pet = Pet("fish", 10)
 
-print("Pet name:", pet.name)
-print("Energy level:", pet.energy)
+pet.play_with_pet()
+
+print("Energy level:", pet.energy_level)
