c4c5858c4da04b267734ee42ef3707477ef34794
kr31o <abok52806@gmail.com>
2026-04-16 14:01:23 +0300

    version 1

---
+++ main.py
@@ -0,0 +1,6 @@
+from pet import Pet
+
+pet = Pet("cat", 30)
+
+print("Pet name:", pet.name)
+print("Energy level:", pet.energy)
---
+++ pet.py
@@ -0,0 +1,4 @@
+class Pet:
+    def __init__(self, name, energy):
+        self.name = name
+        self.energy = energy
